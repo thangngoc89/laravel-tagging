2d29d7489229f556e7e782ded39ac1cc9acf7dfa
thangngoc89 <thangngoc89@users.noreply.github.com>
2015-01-28 00:10:22 +0700

    Update Slugify for slug creator

--- src/config/config.php
+++ src/config/config.php
@@ -3,7 +3,7 @@
 return array(
 	
 	// value of are passed through this before save of tags
-	'normalizer' => '\Conner\Tagging\TaggingUtil::slug',
+	'normalizer' => '\Cocur\Slugify\Slugify::slugify',
 	
 	// display value of tags are passed through (for front end display)
 	'displayer' => '\Str::title',
